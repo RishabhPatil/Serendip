57373e3ecaec3ca2cc2b1ea64579da6b95c62226
Yashgiri Goswami <yashgirigoswami1@gmail.com>
2018-09-21 20:50:52 -0700

    Webpage started to do: navbar

---
+++ webpage/scripts/index.js
@@ -0,0 +1,16 @@
+
+/*$('#pills-tab li:first-child a').on('click', function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#pills-tab li:nth-child(2) a').on('click', function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#pills-tab li:nth-child(3) a').on('click', function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})*/
+$('#pills-tab li:last-child a').tab('show')
