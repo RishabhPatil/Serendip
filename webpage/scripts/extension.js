3708418311e9c732e4f2d9c4bc5105a50c0188bb
Yashgiri Goswami <yashgirigoswami1@gmail.com>
2018-11-25 17:11:49 -0700

    Extension takes input from extension.csv

--- webpage/scripts/extension.js
+++ webpage/scripts/extension.js
@@ -33,26 +33,128 @@ function view_cards()
 		topics_space.appendChild(words);
 	}
 
-
-	var doc_results = [ ["doc1", 20, 30], ["doc2", 20, 20], ["doc3", 10, 20] ];
-
 	var card, card_doc, card_body;
 	var ranking_space = document.getElementById("ranking");
 	ranking_space.innerHTML = "";
 
-	for(i=0; i<doc_results.length && documents.length!=0; i++)
-	{
-		add_card(doc_results[i]);
-	}
+	d3.csv("Extension.csv", function(error, data) {
+		var documents = new Array();
+		var distinct_doc = new Array();
+		var topics = new Array();
+		var lengths = new Array();
+		var present = false;
 
-	/*for(i=0; i<documents.length && documents.length!=0; i++)
-	{
-		add_bubble()
-	}*/
+		data.forEach(function(d) {
+			documents.push(d.Document);
+			topics.push(d.topics);
+			lengths.push(d.length);
+			present = false;
+			for(i=0; i<distinct_doc.length && !present; i++)
+			{
+				if(distinct_doc[i] == d.Document)
+				{
+					present = true;
+				}
+			}
+			if(present == false)
+			{
+				distinct_doc.push(d.Document);
+			}
+		})
+
+		
+
+		for(i=0; i<distinct_doc.length; i++)
+		{
+			var distinct_lengths = new Array();
+			var distinct_topics = new Array();
+			for(j=0; j<documents.length; j++)
+			{
+				if(distinct_doc[i] == documents[j])
+				{
+					distinct_topics.push(topics[j]);
+					distinct_lengths.push(lengths[j]);
+				}
+			}
+			add_card(distinct_doc[i], distinct_topics, distinct_lengths);
+		}
+	});
+
 	display_bubble_chart();	
 }
 
-function add_card(doc_name)
+function add_card(doc_name, topics, lengths)
+{
+
+	var card, card_doc, card_body, card_header_p, img;
+	var ranking_space = document.getElementById("ranking");
+
+	card = document.createElement("div");
+	card.className = "card my-2";
+	card_header = document.createElement("div");
+	card_header.className = "card-header";
+	card_header_h5 = document.createElement("h5");
+	card_header_h5.innerHTML = doc_name;
+	card_header_h5.style.cssFloat = "right";
+	card_header.appendChild(card_header_h5);
+	card.appendChild(card_header);
+	var chart;
+	chart = document.createElement("div");
+
+	d3.csv("Extension.csv", function(){
+		var document_data = new Array();
+		var obj = {};
+
+		for(i=0; i<topics.length ;i++)
+		{
+			obj = {};
+			obj.topic = topics[i];
+			obj.length = lengths[i];
+			document_data.push(obj);
+		}
+
+		var MaxWidth = 1000;
+		var MaxHeight = 80;
+
+		var margin = {top: 0, right: 10, bottom: 0, left: 10},
+		    width = MaxWidth - margin.left - margin.right,
+		    height = MaxHeight - margin.top - margin.bottom;
+
+		var x1 = d3.scaleBand()
+			.domain(topics)
+			.rangeRound([0, width]);
+
+		var y1 = d3.scaleLinear()
+			.range([80,0]);
+
+		y1.domain([0, d3.max(lengths)]);
+
+		var xAxis1 = d3.axisBottom(x1).ticks(30);
+		var yAxis1 = d3.axisLeft(y1).ticks(20);
+
+		var rankBar = d3.select(chart)
+		    .append("svg")
+			.attr("width", MaxWidth)
+			.attr("height", MaxHeight);
+
+
+		rankBar.selectAll(".bar")
+			.data(document_data)
+			.enter()
+			.append("rect")
+		      .attr("class", "bar")
+		      .attr("x", function(d) { return x1(d.topic) })
+		      .attr("y", function(d) { return y1(d.length) } )
+		      .attr("width", 25)
+		      .attr("height", function(d) { return height - y1(d.length); })
+		      .attr("fill","grey");
+   	});
+
+	card_header.appendChild(chart);
+	ranking_space.appendChild(card);
+}
+
+/*function add_card(doc_name)
 {
 	var card, card_doc, card_body, card_header_p, img;
 	var ranking_space = document.getElementById("ranking");
@@ -116,11 +218,11 @@ function add_card(doc_name)
 		      .attr("fill","grey");
    	});
 
-/*	var dataURL = chart.node().toDataURL();
-	card_header.style('background-image', 'url(' + dataURL + ')');*/
+//	var dataURL = chart.node().toDataURL();
+//	card_header.style('background-image', 'url(' + dataURL + ')');
 	card_header.appendChild(chart);
 	ranking_space.appendChild(card);
-}
+}*/
 
 function display_bubble_chart()
 {
