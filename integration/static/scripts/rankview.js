7644680978e84373f8543d86a60baeeedb8449bd
Nitesh Sarode <sarodenitesh.89@gmail.com>
2018-11-26 04:38:25 -0700

    Added static data in rank view.

--- integration/static/scripts/rankview.js
+++ integration/static/scripts/rankview.js
@@ -1,28 +1,34 @@
-function rankView(DATA_FOLDER, topic_filename){
-	function add_word()
-	{
-		var text, color;
-		text=document.getElementById("inputword").value;
-		color=document.getElementById("Select").value;
-		
-		var words_panel=document.getElementById("words");
-		var para=document.createElement('p');
-		para.innerHTML = text;
-
-		para.style.color=color;
-		words_panel.appendChild(para);
-	}
-
-	var filename = DATA_FOLDER+"TopicsWords.csv";
+function add_word()
+{
+	var text, color;
+	text=document.getElementById("inputword").value;
+	color=document.getElementById("Select").value;
 	
+	var words_panel=document.getElementById("words");
+	var para=document.createElement('p');
+	para.innerHTML = text;
+
+	para.style.color=color;
+	words_panel.appendChild(para);
+}
+
+function rankView(DATA_FOLDER, topic_filename){
+
+	var filename = DATA_FOLDER+"TopicWords.csv";
+
+	var word_data_1 = [0.95,0.75,0.4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+	var word_data_2 = [0.8,0.85,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
 	d3.csv(filename, function(error, data) {
 		if (error) throw error;
 
 		var topics = new Array();
-		var lengths = new Array()
+		var lengths = new Array();
+		var ranks = new Array();
 		data.forEach(function(d) {
 			topics.push(d.topics);
-			lengths.push(d.length);
+			lengths.push(100);
+			ranks.push(d.rank);
 		})
 
 		var margin = {top: 30, right: 50, bottom: 150, left: 150},
@@ -57,14 +63,14 @@ function rankView(DATA_FOLDER, topic_filename){
 		      .attr("dy", "0em")
 		      .attr("transform", "rotate(90)" );
 
-		rankBar.append("g")
-		      .attr("class", "y axis")
-		      .call(yAxis1)
-		    .append("text")
-		      .attr("transform", "rotate(-90)")
-		      .attr("y", 6)
-		      .attr("dy", ".71em")
-		      .style("text-anchor", "end");
+		// rankBar.append("g")
+		//       .attr("class", "y axis")
+		//       .call(yAxis1)
+		//     .append("text")
+		//       .attr("transform", "rotate(-90)")
+		//       .attr("y", 6)
+		//       .attr("dy", ".71em")
+		//       .style("text-anchor", "end");
 
 
 		rankBar.selectAll(".bar")
@@ -78,15 +84,28 @@ function rankView(DATA_FOLDER, topic_filename){
 		      .attr("height", function(d) { return height - y1(d.length); })
 		      .attr("fill","grey");
 
-		rankBar.append("line")
-			.data(data)
-			.attr("dy", ".35em")
-			.attr("x1", function(d) { return x1(d.topics) })
-			.attr("y1", function(d) { return height - y1(d.length) - 40 })
-			.attr("x2", function(d) { return x1(d.topics) + 15 })
-			.attr("y2", function(d) { return height - y1(d.length) - 40 })
-			.attr("stroke-width", 2.0)
-			.attr("stroke", "black")
+		for (var i = 0; i < topics.length; i++) {
+			if(word_data_1[i] != 0) {
+				rankBar.append("line")
+						.data(data)
+						.attr("x1", function(d) { return x1(d.topics) + (i*19) })
+						.attr("y1", function(d) { return height - y1(d.length) - (word_data_1[i]*160); })
+						.attr("x2", function(d) { return x1(d.topics) + (i*19) + 15 })
+						.attr("y2", function(d) { return height - y1(d.length) - (word_data_1[i]*160) })
+						.attr("stroke-width", 2.0)
+						.attr("stroke", "red");
+			}
+			if(word_data_2[i] != 0) {
+				rankBar.append("line")
+						.data(data)
+						.attr("x1", function(d) { return x1(d.topics) + (i*19) })
+						.attr("y1", function(d) { return height - y1(d.length) - (word_data_2[i]*160); })
+						.attr("x2", function(d) { return x1(d.topics) + (i*19) + 15 })
+						.attr("y2", function(d) { return height - y1(d.length) - (word_data_2[i]*160) })
+						.attr("stroke-width", 2.0)
+						.attr("stroke", "blue");
+			}
+		}
    	});
 
 }
