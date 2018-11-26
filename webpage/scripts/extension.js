bd50ce4d1f5c292d0352ab62871b3b7f92d5971b
Yashgiri Goswami <yashgirigoswami1@gmail.com>
2018-11-26 00:31:28 -0700

    Adding bubble_chart js
    fixes to index.js
    fixes in UI for Corpus
    Major changes for extension.js and bubble_chart

--- webpage/scripts/extension.js
+++ webpage/scripts/extension.js
@@ -4,22 +4,41 @@ var documents = new Array;
 function add_word_extension()
 {
 	var text;
-	text = document.getElementById("inputword2").value;
-	if(text != "")
+	var text_inputbar = document.getElementById("inputword2");
+	text = text_inputbar.value;
+	if(text == "")
 	{
+		alert("Empty text box");
+		return;
+	}
+	if(text != "")
+	{	
+		for(i=0; i<documents.length; i++)
+		{
+			if(text == documents[i])
+			{
+				alert("Already added");
+				return;
+			}
+		}
+
 		documents.push(text);	
 	}
-	view_cards();
+	text_inputbar.value = "";
+	view_cards_and_chart();
 }
 
 function clear_word_extension()
 {
 	document.getElementById("inputword2").value = "";
 	documents = [];
-	view_cards();
+	view_cards_and_chart();
+
+	var bubble_chartspace = document.getElementById("bubble");
+    bubble_chartspace.innerHTML = "";
 }
 
-function view_cards()
+function view_cards_and_chart()
 {
 	var topics_space = document.getElementById("topics");
 	topics_space.innerHTML = "";
@@ -27,8 +46,8 @@ function view_cards()
 
 	for(i=0; i<documents.length; i++)
 	{
-		words=document.createElement('button');
-		words.class = "btn btn-primary mx-2";
+		words=document.createElement("button");
+		words.class = "btn btn-sm btn-success";
 		words.innerHTML = documents[i];
 		topics_space.appendChild(words);
 	}
@@ -38,14 +57,14 @@ function view_cards()
 	ranking_space.innerHTML = "";
 
 	d3.csv("Extension.csv", function(error, data) {
-		var documents = new Array();
+		var documents_name = new Array();
 		var distinct_doc = new Array();
 		var topics = new Array();
 		var lengths = new Array();
 		var present = false;
 
 		data.forEach(function(d) {
-			documents.push(d.Document);
+			documents_name.push(d.Document);
 			topics.push(d.topics);
 			lengths.push(d.length);
 			present = false;
@@ -68,9 +87,9 @@ function view_cards()
 		{
 			var distinct_lengths = new Array();
 			var distinct_topics = new Array();
-			for(j=0; j<documents.length; j++)
+			for(j=0; j<documents_name.length; j++)
 			{
-				if(distinct_doc[i] == documents[j])
+				if(distinct_doc[i] == documents_name[j])
 				{
 					distinct_topics.push(topics[j]);
 					distinct_lengths.push(lengths[j]);
@@ -80,7 +99,29 @@ function view_cards()
 		}
 	});
 
-	display_bubble_chart();	
+	var topics = new Array();
+	var importance = new Array();
+
+	//Get the topics from the TopicsWords.csv
+	d3.csv("TopicsWords.csv", function(error, data)
+	{
+		data.forEach(function(d)
+		{
+			for(i=0; i<documents.length; i++)
+			{
+				if(d.topics == documents[i])
+				{
+					topics.push(d.topics);
+					importance.push(d.length);					
+				}
+			}
+		});
+
+
+		display_bubble_chart(topics, importance);	
+	});
+
+	
 }
 
 function add_card(doc_name, topics, lengths)
@@ -154,77 +195,73 @@ function add_card(doc_name, topics, lengths)
 	ranking_space.appendChild(card);
 }
 
-/*function add_card(doc_name)
-{
-	var card, card_doc, card_body, card_header_p, img;
-	var ranking_space = document.getElementById("ranking");
-
-	card = document.createElement("div");
-	card.className = "card my-2";
-	card_header = document.createElement("div");
-	card_header.className = "card-header";
-	card_header_h5 = document.createElement("h5");
-	card_header_h5.innerHTML = doc_name[0];
-	card_header_h5.style.cssFloat = "right";
-	card_header.appendChild(card_header_h5);
-	card.appendChild(card_header);
-	var chart;
-	chart = document.createElement("div");
-
-	d3.csv("TopicsWords.csv", function(error, data) {
-		if (error) throw error;
-
-		var topics = new Array();
-		var lengths = new Array()
-		data.forEach(function(d) {
-			topics.push(d.topics);
-			lengths.push(d.length);	
-		})
-
-		var MaxWidth = 1000;
-		var MaxHeight = 80;
-
-		var margin = {top: 0, right: 10, bottom: 0, left: 10},
-		    width = MaxWidth - margin.left - margin.right,
-		    height = MaxHeight - margin.top - margin.bottom;
-
-		var x1 = d3.scaleBand()
-			.domain(topics)
-			.rangeRound([0, width]);
-
-		var y1 = d3.scaleLinear()
-			.range([80,0]);
-
-		y1.domain([0, d3.max(lengths)]);
-
-		var xAxis1 = d3.axisBottom(x1).ticks(30);
-		var yAxis1 = d3.axisLeft(y1).ticks(20);
-
-		var rankBar = d3.select(chart)
-		    .append("svg")
-			.attr("width", MaxWidth)
-			.attr("height", MaxHeight);
-
-
-		rankBar.selectAll(".bar")
-			.data(data)
-			.enter()
-			.append("rect")
-		      .attr("class", "bar")
-		      .attr("x", function(d) { return x1(d.topics) })
-		      .attr("y", function(d) { return y1(d.length) } )
-		      .attr("width", 25)
-		      .attr("height", function(d) { return height - y1(d.length); })
-		      .attr("fill","grey");
-   	});
-
-//	var dataURL = chart.node().toDataURL();
-//	card_header.style('background-image', 'url(' + dataURL + ')');
-	card_header.appendChild(chart);
-	ranking_space.appendChild(card);
-}*/
-
-function display_bubble_chart()
+function display_bubble_chart(topics, importance)
 {
 	
+    var bubble_chartspace = document.getElementById("bubble");
+    bubble_chartspace.innerHTML = "";
+
+	var chart = document.createElement("div");
+
+	
+	alert(topics);
+
+	/*var data = new Array();
+	var obj = {}
+	for(i=0; i<topics.length; i++)
+	{
+		obj = {};
+		obj.topic = topics[i];
+		obj.importance = importance[i];
+		data.push(obj);
+	}
+
+	
+	//Start with the chart dimensions
+
+	var diameter = 500,
+		pad = 1.5,
+		color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+	alert("1 here");
+
+	var bubble = d3.pack(data)
+					.size([diameter, diameter])
+					.padding(pad);
+
+	alert("2");
+	var svg = d3.select(chart)
+            .append("svg")
+            .attr("width", diameter)
+            .attr("height", diameter)
+            .attr("class", "bubble");
+
+    alert("3");
+    var nodes = d3.hierarchy(data)
+        .sum(function(d) { return d.importance; });
+
+    alert("4");
+        var node = svg.selectAll(".node")
+            .data(bubble(nodes).descendants())
+            .enter()
+            .filter(function(d){
+                return  !d.children
+            })
+            .append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+
+    alert("5");
+        node.append("circle")
+            .attr("r", function(d) {
+                return d.importance;
+            })
+            .style("fill", function(d,i) {
+                return color(i);
+            });*/
+    
+    bubble_chartspace.appendChild(chart);
+
 }
--- webpage/scripts/index.js
+++
@@ -1,6 +0,0 @@
-
-function close_window(){
-	//var mp = this.parentNode.parentNode.parentNode;
-	alert(this.id);
-	//this.parentNode.parentNode.style.display="none";
-}
